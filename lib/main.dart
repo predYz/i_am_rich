6644454b2ba62f364dd9b6b028a75fcd171f5848
predYz <83245515+predYz@users.noreply.github.com>
2022-06-23 17:21:06 +0200

    First flutter app from Dr.Angela Yu via udemy.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point of all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I will be Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
