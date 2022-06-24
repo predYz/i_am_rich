d14d557cd3cffc73d8a30ee9b95aaff72f2811fe
predYz <83245515+predYz@users.noreply.github.com>
2022-06-24 13:53:38 +0200

    Added AssetImage and change the title of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Center(child: Text('I will be Rich')),
+          title: Center(child: Text('Rich life means Happy life')),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            image: AssetImage('images/redtree.png'),
           ),
         ),
       ),
